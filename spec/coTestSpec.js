e6d97e3df1da954f422f489fbe658a778185f355
Kelvis Artigas <kartigas@smartboxtv.com>
2019-08-31 14:23:02 -0400

    - Adding test cases to keep full coverage code and use cases

--- spec/coTestSpec.js
+++ spec/coTestSpec.js
@@ -1,14 +1,130 @@
 const expect = require('chai').expect;
 
-const coTest = require('../src/coTest');
-const CarInsurance = coTest.CarInsurance;
-const Product = coTest.Product;
+const { CarInsurance, Product } = require('../src/coTest');
 
-describe('Co Test', function() {
-    it('should foo', function() {
-        const coTest = new CarInsurance([ new Product('foo', 0, 0) ]);
-        const products = coTest.updatePrice();
+describe('CO Test', () => {
+    it('Empty Products', () => {
+        const coTest = new CarInsurance();
 
-        expect(products[0].name).equal('fixme');
+        expect(coTest.products).length(0);
+    });
+
+    it('First day 0 to 1', () => {
+        const infoProducts = require('./seeds/testOne');
+        const arrayOfProducts = infoProducts.map(
+            product => new Product(
+                product.name,
+                product.sellIn,
+                product.price
+            )
+        );
+        const coTest = new CarInsurance(arrayOfProducts);
+
+        coTest.updatePrice().forEach((product, i) => {
+            expect(product.sellIn).equal(infoProducts[i].expected.sellIn);
+            expect(product.price).equal(infoProducts[i].expected.price);
+        });
+    });
+
+    it('sellIns Expired', () => {
+        const infoProducts = require('./seeds/testTwo');
+        const arrayOfProducts = infoProducts.map(
+            product => new Product(
+                product.name,
+                product.sellIn,
+                product.price
+            )
+        );
+        const coTest = new CarInsurance(arrayOfProducts);
+
+        coTest.updatePrice().forEach((product, i) => {
+            expect(product.sellIn).equal(infoProducts[i].expected.sellIn);
+            expect(product.price).equal(infoProducts[i].expected.price);
+        });
+    });
+
+    it('Max Prices, never more that 50', () => {
+        const infoProducts = require('./seeds/testThree');
+        const arrayOfProducts = infoProducts.map(
+            product => new Product(
+                product.name,
+                product.sellIn,
+                product.price
+            )
+        );
+        const coTest = new CarInsurance(arrayOfProducts);
+
+        coTest.updatePrice().forEach((product, i) => {
+            expect(product.sellIn).equal(infoProducts[i].expected.sellIn);
+            expect(product.price).equal(infoProducts[i].expected.price);
+        });
+    });
+
+    it('Low prices, never should be 0', () => {
+        const infoProducts = require('./seeds/testFour');
+        const arrayOfProducts = infoProducts.map(
+            product => new Product(
+                product.name,
+                product.sellIn,
+                product.price
+            )
+        );
+        const coTest = new CarInsurance(arrayOfProducts);
+
+        coTest.updatePrice().forEach((product, i) => {
+            expect(product.sellIn).equal(infoProducts[i].expected.sellIn);
+            expect(product.price).equal(infoProducts[i].expected.price);
+        });
+    });
+
+    it('Mega Coverage', () => {
+        const infoProducts = require('./seeds/testFive');
+        const arrayOfProducts = infoProducts.map(
+            product => new Product(
+                product.name,
+                product.sellIn,
+                product.price
+            )
+        );
+        const coTest = new CarInsurance(arrayOfProducts);
+
+        coTest.updatePrice().forEach((product, i) => {
+            expect(product.sellIn).equal(infoProducts[i].expected.sellIn);
+            expect(product.price).equal(infoProducts[i].expected.price);
+        });
+    });
+
+    it('Full Coverage', () => {
+        const infoProducts = require('./seeds/testSix');
+        const arrayOfProducts = infoProducts.map(
+            product => new Product(
+                product.name,
+                product.sellIn,
+                product.price
+            )
+        );
+        const coTest = new CarInsurance(arrayOfProducts);
+
+        coTest.updatePrice().forEach((product, i) => {
+            expect(product.sellIn).equal(infoProducts[i].expected.sellIn);
+            expect(product.price).equal(infoProducts[i].expected.price);
+        });
+    });
+
+    it('Special Full Coverage', () => {
+        const infoProducts = require('./seeds/testSeven');
+        const arrayOfProducts = infoProducts.map(
+            product => new Product(
+                product.name,
+                product.sellIn,
+                product.price
+            )
+        );
+        const coTest = new CarInsurance(arrayOfProducts);
+
+        coTest.updatePrice().forEach((product, i) => {
+            expect(product.sellIn).equal(infoProducts[i].expected.sellIn);
+            expect(product.price).equal(infoProducts[i].expected.price);
+        });
     });
 });
