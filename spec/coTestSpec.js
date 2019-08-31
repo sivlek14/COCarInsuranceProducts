b7ac1218c91d9ea1a7405affcc43b74bd1ebe583
Kelvis Artigas <kartigas@smartboxtv.com>
2019-08-31 12:55:04 -0400

    - Clean code

--- spec/coTestSpec.js
+++ spec/coTestSpec.js
@@ -4,12 +4,11 @@ const coTest = require('../src/coTest');
 const CarInsurance = coTest.CarInsurance;
 const Product = coTest.Product;
 
-describe("Co Test", function() {
+describe('Co Test', function() {
+    it('should foo', function() {
+        const coTest = new CarInsurance([ new Product('foo', 0, 0) ]);
+        const products = coTest.updatePrice();
 
-  it("should foo", function() {
-    const coTest = new CarInsurance([ new Product("foo", 0, 0) ]);
-    const products = coTest.updatePrice();
-    expect(products[0].name).equal("fixme");
-  });
-
+        expect(products[0].name).equal('fixme');
+    });
 });
